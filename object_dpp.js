a0caab8cc037c2e1d33748a15d817bf968511d8d
titulus desiderio <titulus.web@gmail.com>
2016-07-27 08:55:47 +0300

    fix the previous crazy idea-commit

--- object_dpp.js
+++ object_dpp.js
@@ -1,19 +1,18 @@
 function Object_defineProtectedProperty(obj,prop,descriptor) {
 	function isCalledInbound(method) {
 		for (let i in obj) {
-	 		if (!obj.hasOwnProperty(i)) continue;
 	 		if (method.caller === obj[i]) return true;
 	 	};
 	 	return false;
 	};
-	let protectedProperties = new WeakMap();
+	let protectedProperty = (descriptor)?descriptor.value:undefined;
 	Object.defineProperty(obj,prop,{
 		 configurable: false
 		,get:function getProperty() {
-		 	if (isCalledInbound(getProperty)) return protectedProperties.get(this);
+		 	if (isCalledInbound(getProperty)) return protectedProperty;
 		}
 		,set:function setProperty(value) {
-		 	if (isCalledInbound(setProperty)) protectedProperties.set(this,value);
+		 	if (isCalledInbound(setProperty)) protectedProperty = value;
 		}
 	});
 };
